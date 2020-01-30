824b674e0e32ac3b0fb9cc1583afa3097facbb9f
minjasilven <minja.silvennoinen@futurice.com>
2020-01-30 17:51:17 +0200

    Started time handler

--- Assets/_Scripts/General/GameManager.cs
+++ Assets/_Scripts/General/GameManager.cs
@@ -8,4 +8,10 @@ using System.Collections;
 public class GameManager : MonoBehaviour
 {
     private Inventory _inventory = new Inventory();
+    private TimeHandler _timeHandler = new TimeHandler();
+
+    void Start()
+    {
+        Debug.Log(_timeHandler.GetCurrentTime());
+    }
 }
--- Assets/_Scripts/General/TimeHandler.cs
+++ Assets/_Scripts/General/TimeHandler.cs
@@ -1,8 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TimeHandler
 {
-    
+    private DateTime _currentTime = DateTime.Now;
+    private int _currentHour = 0;
+    private int _currentMinute = 0;
+    private string _format = "HH:mm";
+
+    public TimeHandler()
+    {
+        UpdateCurrentTime();
+    }
+
+    public void UpdateCurrentTime()
+    {
+        var timeNow = DateTime.Now;
+
+        _currentHour = timeNow.Hour;
+        _currentMinute = timeNow.Minute;
+    }
+
+    public DateTime GetCurrentTime()
+    {
+        return _currentTime;
+    }
 }
